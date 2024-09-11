8e71f3eeeaeed58e60651794b7a691e78759297b
Beatriz Varela <beatrizsv19@gmail.com>
2024-09-11 19:48:51 -0300

    Criação do backend da aplicação

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,33 @@
 import fastify from 'fastify';
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { createGoal } from '../functions/create-goal';
 import z from 'zod';
-import { title } from 'process';
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.post('/goals', async (request) => {
-    const createGoalSchema = z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-    })
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-    const body = createGoalSchema.parse(request.body)
+app.post('/goals', {
+    schema: {
+        body: z.object({
+            title: z.string(),
+            desiredWeeklyFrequency: z.number().int().min(1).max(7),
+        })
+    }
+} , async request => {
+    const { title, desiredWeeklyFrequency } = request.body
 
     await createGoal({
-        title: body.title,
-        desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+        title,
+        desiredWeeklyFrequency,
     })
 })
 
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log('HTTP server running!');
-})
+app
+    .listen({
+        port: 3333,
+    })
+    .then(() => {
+        console.log('HTTP server running!');
+    })
